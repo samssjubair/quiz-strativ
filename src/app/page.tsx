60d88500f0561baceb4982f8ea76647dc41d21ed
samssjubair <samssjubair@gmail.com>
2024-02-29 15:35:48 +0600

    adding hero section animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ import { useSession } from "next-auth/react";
 import { redirect } from "next/dist/server/api-utils";
 import Spinner from "@/components/Ui/Spinner";
 import { useRouter } from "next/navigation";
+import { TypewriterEffectSmooth } from "@/components/Ui/TypeWriterAnimationEffect";
+import { heroText } from "@/static/HeroText";
 
 export default function Home() {
   const [modalOpen, setModalOpen] = useState(false);
@@ -20,10 +22,11 @@ export default function Home() {
 
   return (
     <main className="container-center">
+      <TypewriterEffectSmooth words={heroText} />
       {status === "unauthenticated" ? (
         <Button
           borderRadius="1.75rem"
-          className="bg-white font-bold dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800"
+          className="animated-btn"
           onClick={() => setModalOpen(true)}
         >
           See Credentials
@@ -31,7 +34,7 @@ export default function Home() {
       ) : session?.user.role == "admin" ? (
         <Button
           borderRadius="1.75rem"
-          className="bg-white font-bold dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800"
+          className="animated-btn"
           onClick={() => route.push("/admin-panel")}
         >
           Admin Panel
@@ -39,7 +42,7 @@ export default function Home() {
       ) : (
         <Button
           borderRadius="1.75rem"
-          className="bg-white font-bold dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800"
+          className="animated-btn"
           onClick={() => route.push("/user-panel")}
         >
           User Panel
---
+++ src/static/HeroText.ts
@@ -0,0 +1,18 @@
+export const heroText = [
+  {
+    text: "Awsome",
+  },
+  {
+    text: "quiz",
+  },
+  {
+    text: "app",
+  },
+  {
+    text: "for",
+  },
+  {
+    text: "Strativ",
+    className: "text-blue-500 dark:text-blue-500",
+  },
+];
